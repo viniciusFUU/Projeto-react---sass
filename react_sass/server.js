00e77c05fd54a7934a77619319b7a34d6f97f36d
vinicius3.98 <vinicius3_1998@hotmail.com>
2022-11-04 15:56:30 -0300

    atualização 3

--- react_sass/server.js
+++ react_sass/server.js
@@ -1,1 +1,20 @@
-console.log('batata');
+const express = require('express')
+const { resolve } = require('path')
+
+const app = express()
+
+app.use('/', express.static(
+    resolve(
+        __dirname,
+        './dist'
+        )
+    )
+)
+
+app.listen(process.env.PORT || 300, (err) => {
+    if (err) {
+         return console.log(err);
+        } else {
+            return console.log('Tudo funcionando certo.');
+        }
+})
