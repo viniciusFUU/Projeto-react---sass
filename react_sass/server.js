923979d89d28a3362f090190c5d59e30beb14e67
vinicius3.98 <vinicius3_1998@hotmail.com>
2022-11-04 16:01:54 -0300

    atualização 4

--- react_sass/server.js
+++ react_sass/server.js
@@ -1,20 +1,14 @@
-const express = require('express')
-const { resolve } = require('path')
+const express = require("express");
+const { resolve } = require("path");
 
-const app = express()
+const app = express();
 
-app.use('/', express.static(
-    resolve(
-        __dirname,
-        './dist'
-        )
-    )
-)
+app.use("/", express.static(resolve(__dirname, "./build")));
 
 app.listen(process.env.PORT || 300, (err) => {
-    if (err) {
-         return console.log(err);
-        } else {
-            return console.log('Tudo funcionando certo.');
-        }
-})
+  if (err) {
+    return console.log(err);
+  } else {
+    return console.log("Tudo funcionando certo.");
+  }
+});
